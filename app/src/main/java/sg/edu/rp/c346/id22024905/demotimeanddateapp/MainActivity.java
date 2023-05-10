4199bbf59470164935ee4bd5d43763acfb4a8043
22024905@myrp.edu.sg <22024905@myrp.edu.sg>
2023-05-10 23:05:28 +0800

    Initial Commit

--- app/src/main/java/sg/edu/rp/c346/id22024905/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22024905/demotimeanddateapp/MainActivity.java
@@ -3,12 +3,44 @@ package sg.edu.rp.c346.id22024905.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    DatePicker dp;
+    TimePicker tp;
+    Button btnDisplayDate;
+    Button btnDisplayTime;
+    TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                int hour = cal.get(Calendar.HOUR_OF_DAY);
+                int minute = cal.get(Calendar.MINUTE);
+
+                String time = String.format("%d:%.02d", hour, minute);
+
+                tvDisplay.setText(time);
+            }
+        });
     }
+
 }
