ade2261c245bdfee3012e9c794015fc6ffe5f9e5
22024905@myrp.edu.sg <22024905@myrp.edu.sg>
2023-05-11 10:53:05 +0800

    Commit till section C

--- app/src/main/java/sg/edu/rp/c346/id22024905/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22024905/demotimeanddateapp/MainActivity.java
@@ -9,16 +9,20 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.SimpleTimeZone;
 
 public class MainActivity extends AppCompatActivity {
     DatePicker dp;
     TimePicker tp;
     Button btnDisplayDate;
     Button btnDisplayTime;
+
+    Button btnReset;
     TextView tvDisplay;
 
     @Override
@@ -31,27 +35,45 @@ public class MainActivity extends AppCompatActivity {
         btnDisplayDate = findViewById(R.id.buttonDisplayDate);
         btnDisplayTime = findViewById(R.id.buttonDisplayTime);
         tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.buttonReset);
 
         btnDisplayTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int hour = tp.getHour();
                 int min = tp.getMinute();
-                tvDisplay.setText("Time is " + hour + ":" + min);
+                String formattedTime = String.format("%02d:%02d", hour, min);
+                tvDisplay.setText("Time is " + formattedTime);
             }
         });
 
         btnDisplayDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               int DOM = dp.getDayOfMonth();
+                String a = new String();
+                String b = new String();
+
+               int dom = dp.getDayOfMonth();
                int month = dp.getMonth();
                int year = dp.getYear();
-
-               tvDisplay.setText("Date is " + DOM + "/" + month + "/" + year);
+               Calendar calendar = Calendar.getInstance();
+               calendar.set(year, month, dom);
+               SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+               String date = dateFormat.format(calendar.getTime());
+               tvDisplay.setText("Date is " + date);
 
             }
         });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tp.setHour(0);
+                tp.setMinute(0);
+                dp.updateDate(2020, 01, 01);
+            }
+        });
+
     }
 
 }
